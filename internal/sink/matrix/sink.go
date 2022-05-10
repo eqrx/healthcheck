a52f6bf8666fda404acaeeda02357d6b46b530a7
Alexander Sowitzki <dev@eqrx.net>
2022-05-10 13:08:49 +0200

    Update golangci

--- internal/sink/matrix/sink.go
+++ internal/sink/matrix/sink.go
@@ -11,6 +11,7 @@
 // You should have received a copy of the GNU Affero General Public License along with this program.
 // If not, see <https://www.gnu.org/licenses/>.
 
+// Package matrix sinks healthcheck notifications into matrix rooms.
 package matrix
 
 import (
--- internal/sink/sink.go
+++ internal/sink/sink.go
@@ -11,6 +11,7 @@
 // You should have received a copy of the GNU Affero General Public License along with this program.
 // If not, see <https://www.gnu.org/licenses/>.
 
+// Package sink contains sinks for message healthchecks.
 package sink
 
 import (
